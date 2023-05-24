eeb8779f9f9b40601c5d0e685cc726587e026b0f
Henry Fraser <henrymfraser@gmail.com>
2023-05-25 05:54:15 +1000

    Add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import io.gitlab.arturbosch.detekt.Detekt
 plugins {
     kotlin("jvm") version "1.8.21"
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
     application
 }
 
