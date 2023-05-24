0b5b8e5ef25deab296e4656257ebe467cedacbe9
Henry Fraser <frashen@amazon.com>
2023-05-24 11:02:05 +1000

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     kotlin("jvm") version "1.8.21"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
     application
 }
 
@@ -8,6 +11,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+detekt {
+    allRules = false // activate all available (even unstable) rules.
+    buildUponDefaultConfig = true // preconfigure defaults
+    parallel = true
+    config = files("$rootDir/detekt.yml")
+    source = files(projectDir)
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = JavaVersion.VERSION_17.toString()
+    exclude("**/build/**")
+    reports {
+        html.required.set(true)
+        md.required.set(true)
+    }
 }
 
 dependencies {
